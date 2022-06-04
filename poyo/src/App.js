e10e0fdbe00cd28a081a7612cae0edb07da5e255
choisung2 <c0817d@gmail.com>
2022-06-05 01:21:34 +0900

    ✨ router 기본 설정

--- poyo/src/App.js
+++ poyo/src/App.js
@@ -1,8 +1,13 @@
+import { Route, Routes } from "react-router-dom";
+import loadable from "@loadable/component";
 
+const Main = loadable(() => import("./pages/Main"));
 
 function App() {
   return (
-    <></>
+    <Routes>
+      <Route path="/" element={<Main />} />
+    </Routes>
   );
 }
 
---
+++ poyo/src/components/home/Home.js
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <>
+      <h1>홈 화면 입니다!</h1>
+    </>
+  );
+}
