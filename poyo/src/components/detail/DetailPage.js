61108b597be3140b1740d9b757bf2207371b2e86
Mangopapa1 <gustp26@gmail.com>
2022-06-06 17:49:43 +0900

    🔥 중복된 폴더 삭제

--- poyo/src/components/detail/DetailPage.js
+++
@@ -1,8 +0,0 @@
-export default function DetailPage() {
-    return (
-      <>
-        <h1>상세 페이지 입니다!</h1>
-      </>
-    );
-  }
-  
--- poyo/src/components/home/Home.js
+++
@@ -1,7 +0,0 @@
-export default function Home() {
-  return (
-    <>
-      <h1>홈 화면 입니다!</h1>
-    </>
-  );
-}
