cdecbf21fadc9b494cd58cdb62a06dd40b1fd3f5
choisung2 <c0817d@gmail.com>
2022-06-08 16:34:20 +0900

    ✨ 검색 결과 페이지 동적 라우팅 연결

--- FrontEnd/poyo/src/App.js
+++ FrontEnd/poyo/src/App.js
@@ -3,12 +3,14 @@ import loadable from "@loadable/component";
 import "./styles/globals.css";
 
 const Main = loadable(() => import("./pages/Main"));
+const Search = loadable(() => import("./pages/Search"));
 const Detail = loadable(() => import("./pages/Detail"));
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Main />} />
+      <Route path="/search/:id" element={<Search />} />
       <Route path="/detail" element={<Detail />} />
     </Routes>
   );
