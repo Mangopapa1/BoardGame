ed8dc9137526a7e16642d2ada99756d5e5286ca1
dundegi <sinpno@naver.com>
2022-07-08 00:33:46 +0900

    :art: Repository, Service 작성

--- BackEnd/src/main/java/BoardGame/BackEnd/Repository/BoardGameRepository.java
+++ BackEnd/src/main/java/BoardGame/BackEnd/Repository/BoardGameRepository.java
@@ -1,4 +1,36 @@
 package BoardGame.BackEnd.Repository;
 
+import BoardGame.BackEnd.Entity.BoardGame;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import java.util.List;
+
+@Repository
+@RequiredArgsConstructor
 public class BoardGameRepository {
+
+    private final EntityManager em;
+
+    //보드게임 저장
+    public void save(BoardGame boardGame){
+        em.persist(boardGame);
+    }
+
+    //보드게임 검색
+    public BoardGame findOne(String name){
+        return em.find(BoardGame.class, name);
+
+    }
+    //장르별 검색
+    public List<BoardGame> findType(){
+        return em.createQuery("select m from BoardGame where type = ?").getResultList();
+    }
+
+    //난이도 별검색
+    public List<BoardGame> findDifficulty(){
+        return em.createQuery("select m from BoardGame where difficulty = ?").getResultList();
+    }
+
 }
--- BackEnd/src/main/java/BoardGame/BackEnd/Service/BoardGameService.java
+++ BackEnd/src/main/java/BoardGame/BackEnd/Service/BoardGameService.java
@@ -1,4 +1,33 @@
 package BoardGame.BackEnd.Service;
 
+import BoardGame.BackEnd.Entity.BoardGame;
+import BoardGame.BackEnd.Repository.BoardGameRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
 public class BoardGameService {
+
+    private final BoardGameRepository boardGameRepository;
+
+    public List<BoardGame> findType(){
+        return boardGameRepository.findType();
+    }
+
+    public List<BoardGame> findDifficulty(){
+        return boardGameRepository.findDifficulty();
+    }
+
+    public BoardGame findOne(String name){
+        return boardGameRepository.findOne(name);
+    }
+
+
+
+
 }
