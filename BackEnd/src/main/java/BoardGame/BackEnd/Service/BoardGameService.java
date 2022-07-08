48bb4f29b1fa38f03d930e88fdf6bb7294c6608b
dundegi <sinpno@naver.com>
2022-07-08 20:41:47 +0900

    :art: 레포지토리 및 서비스 작성

--- BackEnd/src/main/java/BoardGame/BackEnd/Service/BoardGameService.java
+++ BackEnd/src/main/java/BoardGame/BackEnd/Service/BoardGameService.java
@@ -1,4 +1,30 @@
 package BoardGame.BackEnd.Service;
 
+import BoardGame.BackEnd.Entity.BoardGame;
+import BoardGame.BackEnd.Repository.BoardGameRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
 public class BoardGameService {
+
+    private final BoardGameRepository boardGameRepository;
+
+    public BoardGame findOne(String name){
+        return boardGameRepository.findByName(name);
+    }
+
+    public List<BoardGame> findType(String type){
+        return boardGameRepository.findByTypeContaning(type);
+    }
+
+    public List<BoardGame> findDifficulty(String difficulty){
+        return boardGameRepository.findByDifficultyContaning(difficulty);
+    }
 }
