6bfc1edb23ad2aa18e7ff2dd47e7cda9c0776817
kimsd803 <kimsd9683@gmail.com>
2022-07-10 19:45:17 +0900

    리뷰 repository 커밋

--- BackEnd/src/main/java/BoardGame/BackEnd/Repository/ReviewRepository.java
+++ BackEnd/src/main/java/BoardGame/BackEnd/Repository/ReviewRepository.java
@@ -1,27 +1,28 @@
 package BoardGame.BackEnd.Repository;
 
-import BoardGame.BackEnd.DTO.Review;
+import BoardGame.BackEnd.Entity.Review;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import javax.persistence.EntityManager;
 import java.util.List;
 
 @Repository
-@RequiredArgsConstructor
-public class ReviewRepository {
+public interface ReviewRepository extends JpaRepository<Review,String> {
+    List<Review> findByBoardGameId(String boardGameId);
 
-    private final EntityManager em;
 
-    public void save(Review review){
-        em.persist(review);
-    }
-
-    public Review findOne(Long reviewId){
-        return em.find(Review.class, reviewId);
-    }
-
-    public List<Review> findAll(){
-        return em.createQuery("select m from Review m", Review.class).getResultList();
-    }
+//    private final EntityManager em;
+//
+//    public void save(ReviewDto review){
+//        em.persist(review);
+//    }
+//
+//    public ReviewDto findOne(Long reviewId){
+//        return em.find(ReviewDto.class, reviewId);
+//    }
+//
+//    public List<ReviewDto> findAll(){
+//        return em.createQuery("select m from Review m", ReviewDto.class).getResultList();
+//    }
 }
