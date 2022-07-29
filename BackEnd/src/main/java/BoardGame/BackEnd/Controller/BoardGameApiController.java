1fc9d47b61cfa1343be1154f28ca6495a2975f76
dundegi <sinpno@naver.com>
2022-07-29 23:53:41 +0900

    :recycle: refactoring

--- BackEnd/src/main/java/BoardGame/BackEnd/Controller/BoardGameApiController.java
+++ BackEnd/src/main/java/BoardGame/BackEnd/Controller/BoardGameApiController.java
@@ -3,6 +3,7 @@ package BoardGame.BackEnd.Controller;
 import BoardGame.BackEnd.Entity.BoardGame;
 import BoardGame.BackEnd.Repository.BoardGameRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,38 +14,33 @@ import java.util.Optional;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/poyo")
 public class BoardGameApiController {
 
     private final BoardGameRepository boardGameRepository;
 
 
 
-    @GetMapping("/test")
-    public String test(){
 
-        return "test";
-    }
 
-    @GetMapping("/games") //모든게임조회
+    @GetMapping("/poyo/games") //모든게임조회
     public List<BoardGame> AllGames(){
         return boardGameRepository.findAll();
     }
 
-    @GetMapping("/search/{name}") //이름
-    public Optional<BoardGame> findName(@PathVariable String name){
-        return boardGameRepository.findByNameLike(name);
+    @GetMapping("/poyo/search/{name}") //이름
+    public List<BoardGame> findByName(@PathVariable String name){
+        return boardGameRepository.findByName(name);
     }
 
-    @GetMapping("/search/{category}") //장르별
-    public List<BoardGame> findCategory(@PathVariable String category){
-        return boardGameRepository.findByCategoryLike(category);
+    @GetMapping("/poyo/search/{type}") //장르별
+    public List<BoardGame> findByType(@PathVariable String type){
+        return boardGameRepository.findByType(type);
 
     }
 
-    @GetMapping("/search/{difficulty}") //난이도
-    public List<BoardGame> findDifficulty(@PathVariable String difficulty){
-        return boardGameRepository.findByDifficultyLike(difficulty);
+    @GetMapping("/poyo/search/{difficulty}") //난이도
+    public List<BoardGame> findByDifficulty(@PathVariable String difficulty){
+        return boardGameRepository.findByDifficulty(difficulty);
     }
 
     }
--- BackEnd/src/main/java/BoardGame/BackEnd/Repository/BoardGameRepository.java
+++ BackEnd/src/main/java/BoardGame/BackEnd/Repository/BoardGameRepository.java
@@ -2,6 +2,8 @@ package BoardGame.BackEnd.Repository;
 
 import BoardGame.BackEnd.Entity.BoardGame;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -10,13 +12,19 @@ import java.util.Optional;
 @Repository
 public interface BoardGameRepository extends JpaRepository<BoardGame, Long> {
 
-    Optional<BoardGame> findByNameLike(String name); //이름 검색
+    @Query("select m from BoardGame m where m.name like %:name%")
+    List<BoardGame> findByName(@Param("name") String name); //이름 검색
 
-    List<BoardGame> findByDifficultyLike(String difficulty); //난이도 검색
 
-    List<BoardGame> findByCategoryLike(String category); //장르별 검색
-    
-    List<BoardGame> findByPlayersLike(String players); //인원별 검색
+
+    @Query("select m from BoardGame m where m.type like %:type%")
+    List<BoardGame> findByType(@Param("type") String type); //장르별 검색
+
+    @Query("select m from BoardGame m where m.players like %:players%")
+    List<BoardGame> findByPlayers(@Param("players") String players); //인원별 검색
+
+    @Query("select m from BoardGame m where m.difficulty like %:difficulty%")
+    List<BoardGame> findByDifficulty(@Param("difficulty") String difficulty); //난이도 검색
 
 
 
