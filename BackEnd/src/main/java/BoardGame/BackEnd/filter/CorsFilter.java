c4fe29b48f599904ffa5f0679e2a40b771fd9695
dundegi <sinpno@naver.com>
2022-08-16 00:10:02 +0900

    :art: cors config 수정

--- BackEnd/src/main/java/BoardGame/BackEnd/filter/CorsFilter.java
+++ BackEnd/src/main/java/BoardGame/BackEnd/filter/CorsFilter.java
@@ -23,7 +23,7 @@ public class CorsFilter implements Filter {
         HttpServletRequest req = (HttpServletRequest) request;
         HttpServletResponse res = (HttpServletResponse) response;
 
-        res.setHeader("Access-Control-Allow-Origin", "*");
+        res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
         res.setHeader("Access-Control-Allow-Origin-Credentials", "true");
         res.setHeader("Access-Control-Allow-Origin-Methods", "*");
         res.setHeader("Access-Control-Max-Age", "3600");
