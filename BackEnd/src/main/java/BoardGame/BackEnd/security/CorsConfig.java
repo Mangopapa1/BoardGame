8d4997c5378f63978c9e5136ef793e168dcb0186
dundegi <sinpno@naver.com>
2022-08-16 20:59:59 +0900

    :art: cors config 수정

--- BackEnd/src/main/java/BoardGame/BackEnd/security/CorsConfig.java
+++ BackEnd/src/main/java/BoardGame/BackEnd/security/CorsConfig.java
@@ -2,19 +2,19 @@ package BoardGame.BackEnd.security;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
+@EnableWebMvc
 public class CorsConfig implements WebMvcConfigurer {
 
 
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000", "http://54.180.58.142:8080",
-                        "http://localhost:8080",
-                        "https://localhost:3000",
-                        "http://127.0.0.1:3000")
-                .allowedMethods("GET")
+                .allowedOrigins("http://localhost:3000")
+
+                .allowedMethods("*")
                 .allowedHeaders("*");
     }
 
