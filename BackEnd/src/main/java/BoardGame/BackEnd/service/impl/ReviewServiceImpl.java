464f931dd95239ada412fc157ada0948a8395df3
dundegi <sinpno@naver.com>
2022-09-22 14:09:04 +0900

    :art: service 수정

--- BackEnd/src/main/java/BoardGame/BackEnd/service/impl/ReviewServiceImpl.java
+++ BackEnd/src/main/java/BoardGame/BackEnd/service/impl/ReviewServiceImpl.java
@@ -8,6 +8,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Optional;
@@ -24,6 +25,7 @@ public class ReviewServiceImpl implements ReviewService {
 //    private final BCryptModule bCryptModule;
 
     @Override
+    @Transactional
     public ReviewDto insertReview(ReviewDto dto,Long boardGameId) throws Exception {
 
         if (dto.getReplyContent().equals("")) {
